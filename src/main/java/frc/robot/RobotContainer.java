36931c1009adcff901020e8e4c6476e598fe0bbe
r-wallis <55216560+r-wallis@users.noreply.github.com>
2023-01-12 20:30:29 -0500

    2023 makeover
    no commands were changed from 2022

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,54 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.BlingSetCommand;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.Bling;
+import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.OI;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
 public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  DrivetrainSubsystem m_drivetrainsubsystem = new DrivetrainSubsystem();
+  OI m_OI = new OI();
+  Bling m_bling = new Bling();
+  DriveCommand m_driveCommand = new DriveCommand(m_drivetrainsubsystem, m_OI);
+  BlingSetCommand m_blingSetCommand = new BlingSetCommand(m_bling, m_OI);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    configureBindings();
+    // Configure the button bindings
+    configureButtonBindings();
+    CommandScheduler.getInstance().setDefaultCommand(m_drivetrainsubsystem, m_driveCommand);
+    CommandScheduler.getInstance().setDefaultCommand(m_bling, m_blingSetCommand);
   }
 
-  private void configureBindings() {}
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {}
 
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    // An ExampleCommand will run in autonomous
+    return null;
   }
+
 }
---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,57 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+//import ctre libraries through manage vendor libraries with https://maven.ctr-electronics.com/release/com/ctre/phoenixpro/PhoenixProAnd5-frc2023-latest.json
+
+package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  // Define devices here:
+  public WPI_TalonFX testMotor;
+  public int ticksPerRevolution = 2048;
+
+  public double power = 0.0;
+
+  /** Creates a new DrivetrainSubsystem. */
+  public DrivetrainSubsystem() {
+    // Initialize devices here:
+    testMotor = new WPI_TalonFX(46);
+    testMotor.configFactoryDefault();
+    testMotor.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor);
+    testMotor.setSelectedSensorPosition(0);
+    SmartDashboard.putNumber("Motor Percent Output", getVelocity());
+
+  }
+
+  public double getPosition() {
+    return testMotor.getSelectedSensorPosition() * (2 * Math.PI) / ticksPerRevolution;
+  }
+
+  public double getVelocity() {
+    return testMotor.getSelectedSensorVelocity() * (2 * Math.PI) * 10.0 / ticksPerRevolution;
+  }
+
+  public double getMotorTemperature() {
+    return testMotor.getTemperature();
+  }
+
+  public void setPower(double power) {
+    testMotor.set(ControlMode.PercentOutput, power * .5);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Motor Velocity", getVelocity());
+    SmartDashboard.putNumber("Motor Position", getPosition());
+    SmartDashboard.putNumber("Motor Temperature", getMotorTemperature());
+    power = SmartDashboard.getNumber("Motor Percent Output", power);
+
+  }
+}
